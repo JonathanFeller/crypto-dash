914f656c097df39ee882bb78f5d8336cac40ec40
JonathanRicardo <jonricfel@gmail.com>
2020-08-08 00:08:49 -0300

    Added Settings page and localStoraged

---
+++ src/Settings/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Welcome from './WelcomeMessage';
+import ConfirmButton from './ConfirmButton';
+
+export default () => {
+  return (
+    <div> <Welcome/> <ConfirmButton></ConfirmButton> </div>
+  );
+}
