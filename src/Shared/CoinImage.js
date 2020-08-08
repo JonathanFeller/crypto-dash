95c5afa3fdc1be5c5ecd9c7baec19f3906ab1e64
JonathanRicardo <jonricfel@gmail.com>
2020-08-08 14:00:12 -0300

    Added Coin Spotlight

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 
-export default ({coin, style}) => {
+const CoinImage = styled.img`
+  height: 50px;
+  ${props => props.spotlight && css`
+    display: block;
+    height: 200px;
+    margin: auto;
+  `}
+`;
+
+export default ({ coin, spotlight }) => {
   return (
-    <img 
-      alt={coin.CoinSymbol} 
-      style={style || {height: '50px'}}
+    <CoinImage
+      spotlight={spotlight}
+      alt={coin.CoinSymbol}
       src={`http://cryptocompare.com/${coin.ImageUrl}`}
-     />
+    />
   );
 }
